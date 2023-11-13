e468c28f6f02fa20584752b3bacd91d18f27b8cb
Travis Lamberte <travis@lamberte.com>
2023-11-13 08:43:33 -0800

    add post request for sign in

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const authRoutes = require('./routes/authRoutes')
 
 const app = express()
+
+app.use(authRoutes)
 
 const mongoUri = 'mongodb+srv://admin:passwordpassword@cluster0.hybkl9l.mongodb.net/?retryWrites=true&w=majority'
 mongoose.connect(mongoUri)
