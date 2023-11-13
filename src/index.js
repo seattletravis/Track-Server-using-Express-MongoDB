d6ff3598701caf201398675bac3939a02e9a29e6
Travis Lamberte <travis@lamberte.com>
2023-11-13 05:26:20 -0800

    add listen function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const express = require('espress')
 
 const add = express()
 
-app.get('/',(req,res) => {
-    
+app.get('/', (req,res) => {
+    res.send('Hi there!')
+})
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
 })
