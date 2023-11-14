f2e52dc16306cd5381265a1b43909eb833d8b9f2
Travis Lamberte <travis@lamberte.com>
2023-11-13 16:17:30 -0800

    refactor intents in authRoutes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,17 +6,21 @@ const User = mongoose.model('User')
 const router = express.Router()
 
 router.post('/signup', async (req, res) => {
-    const { email, password } = req.body
+  const { email, password } = req.body
 
-    try{
-        const user = new User({ email, password })
-        await user.save()
-        
-        const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY')
-        res.send({ token })
-    }catch (err) {
-        return res.status(422).send(err.message)
-    }
+  try{
+    const user = new User({ email, password })
+    await user.save()
+    
+    const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY')
+    res.send({ token })
+  }catch (err) {
+    return res.status(422).send(err.message)
+  }
+})
+
+router.post('/signin', (req, res) => {
+
 })
 
 module.exports = router
