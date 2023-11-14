ce4c219bd6fee1ce9c10e9c883b808ae9cd52cbe
Travis Lamberte <travis@lamberte.com>
2023-11-13 16:06:17 -0800

    refactor indentation in User.js

--- src/models/User.js
+++ src/models/User.js
@@ -2,36 +2,42 @@ const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 })
 
 userSchema.pre('save', function(next) {
-    const user = this
-    if (!user.isModified('password')){
-        return next()
+  const user = this
+  if (!user.isModified('password')){
+    return next()
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err)
     }
 
-    bcrypt.genSalt(10, (err, salt) => {
-        if (err) {
-            return next(err)
-        }
-
-        bcrypt.hash(user.password, salt, (err, hash) => {
-            if (err) {
-                return next(err)
-            }
-            user.password = hash
-            next()
-        })
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err)
+      }
+      user.password = hash
+      next()
     })
+  })
 })
+
+userSchema.methods.conparePassword = function(candidatePassword) {
+  return new Promise((resolve, reject) => {
+
+  })
+}
 
 mongoose.model('User', userSchema)
