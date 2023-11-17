e54987360e2db7c9a9245a2c6cb569a13ef1b945
Travis Lamberte <travis@lamberte.com>
2023-11-17 05:53:57 -0800

    add try catch to save instance of track

--- src/routes/TrackRoutes.js
+++ src/routes/TrackRoutes.js
@@ -23,9 +23,13 @@ router.post('/tracks', async (req, res) => {
       .send({ error: 'You must provide a name and locations'} )
   }
 
-  const track = new Track({ name, locations, userId: req.user._id })
-  await track.save()
-  res.send(track)
+  try {
+    const track = new Track({ name, locations, userId: req.user._id })
+    await track.save()
+    res.send(track)
+  } catch (err) {
+    res.status(422).send({ error: err.message })
+  }
 })
 
 
