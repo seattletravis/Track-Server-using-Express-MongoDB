d27cf41fb5caa81bbf56cbe466bb9954e10ddd28
Travis Lamberte <travis@lamberte.com>
2023-11-17 05:52:19 -0800

    add post request to ./tracks

--- src/routes/TrackRoutes.js
+++ src/routes/TrackRoutes.js
@@ -14,5 +14,19 @@ router.get('/tracks', async (req, res) => {
   res.send(tracks)
 })
 
+router.post('/tracks', async (req, res) => {
+  const { name, locations } = req.body
+
+  if (!name || !locations) {
+    return res
+      .status(422)
+      .send({ error: 'You must provide a name and locations'} )
+  }
+
+  const track = new Track({ name, locations, userId: req.user._id })
+  await track.save()
+  res.send(track)
+})
+
 
 module.exports = router
